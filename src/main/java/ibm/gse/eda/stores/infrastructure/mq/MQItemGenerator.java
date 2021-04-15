3e98e6c7b60c7719b05279701d8c408612234389
Rick Osowski <rosowski@gmail.com>
2021-04-15 16:04:09 -0500

    Initial updates for MQ TLS support
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- src/main/java/ibm/gse/eda/stores/infrastructure/mq/MQItemGenerator.java
+++ src/main/java/ibm/gse/eda/stores/infrastructure/mq/MQItemGenerator.java
@@ -1,6 +1,8 @@
 package ibm.gse.eda.stores.infrastructure.mq;
 
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -58,36 +60,64 @@ public class MQItemGenerator {
     @Inject
     @ConfigProperty(name = "app.name", defaultValue = "TestApp")
     public String appName;
-    
+
+    @Inject
+    @ConfigProperty(name = "mq.cipher_suite")
+    public Optional<String> mqCipherSuite;
+
+    @Inject
+    @ConfigProperty(name = "mq.ccdt_url")
+    public Optional<String> mqCcdtUrl;
+
     @Inject
     public StoreRepository storeRepository;
-    
-    private Jsonb parser = JsonbBuilder.create();
 
+    private Jsonb parser = JsonbBuilder.create();
 
     protected JMSProducer producer = null;
     private JMSContext jmsContext = null;
     private Destination destination = null;
     private JmsConnectionFactory cf = null;
     protected Jsonb jsonb = null;
-    
+
 	public List<Item> start(int numberOfRecords) {
         List<Item> items = storeRepository.buildItems(numberOfRecords);
         try {
             jmsContext = buildJMSConnectionSession();
             producer = jmsContext.createProducer();
-    
+
             Multi.createFrom().items(items.stream()).subscribe().with(item -> {
                 sendToMQ(item);
             }, failure -> System.out.println("Failed with " + failure.getMessage()));
         } catch (Exception e) {
-            logger.error(e.getMessage());
+            if (e != null) {
+              if (e instanceof JMSException) {
+                  processJMSException((JMSException) e);
+              } else {
+                  logger.error(e.getMessage());
+                  logger.error(e.getCause());
+              }
+            }
         } finally {
             jmsContext.close();
         }
-       
+
         return items;
 	}
+
+  private static void processJMSException(JMSException jmsex) {
+      logger.info(jmsex.getMessage());
+      Throwable innerException = jmsex.getLinkedException();
+      logger.info("Exception is: " + jmsex);
+      if (innerException != null) {
+          logger.info("Inner exception(s):");
+      }
+      while (innerException != null) {
+          logger.error(innerException.getMessage());
+          innerException = innerException.getCause();
+      }
+      return;
+  }
 
     private void sendToMQ(Item item) {
         String msg = parser.toJson(item);
@@ -95,22 +125,56 @@ public class MQItemGenerator {
         TextMessage message = jmsContext.createTextMessage(msg);
         producer.send(destination, message);
     }
-    
+
+    private String validateCcdtFile(){
+      /*
+       *  Modeled after github.com/ibm-messaging/mq-dev-patterns/blob/master/JMS/com/ibm/mq/samples/jms/SampleEnvSetter.java
+       */
+      String value = mqCcdtUrl.orElse("");
+      String filePath = null;
+      if (value != null && ! value.isEmpty()) {
+        logger.info("Checking for existence of file " + value);
+        File tmp = new File(value);
+        if (! tmp.exists()) {
+          logger.info(value + " does not exist...");
+          filePath = null;
+        } else {
+          logger.info(value + " exists!");
+          filePath = value;
+        }
+      }
+      return filePath;
+    }
+
     private JMSContext buildJMSConnectionSession() throws JMSException {
         JmsFactoryFactory ff = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER);
         cf = ff.createConnectionFactory();
-        cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, this.mqHostname);
-        cf.setIntProperty(WMQConstants.WMQ_PORT, this.mqHostport);
         cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
         cf.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, this.mqQmgr);
         cf.setStringProperty(WMQConstants.WMQ_APPLICATIONNAME, this.appName);
-        cf.setStringProperty(WMQConstants.WMQ_CHANNEL, this.mqChannel);
+
         cf.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true);
         cf.setStringProperty(WMQConstants.USERID, this.mqAppUser);
         cf.setStringProperty(WMQConstants.PASSWORD, this.mqPassword);
+
+        String ccdtFilePath = validateCcdtFile();
+        if(ccdtFilePath==null){
+          logger.info("No valid CCDT file detected. Using host, port, and channel properties instead.");
+          cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, this.mqHostname);
+          cf.setIntProperty(WMQConstants.WMQ_PORT, this.mqHostport);
+          cf.setStringProperty(WMQConstants.WMQ_CHANNEL, this.mqChannel);
+        } else {
+          logger.info("Setting CCDTURL to 'file://"+ccdtFilePath+"'");
+          cf.setStringProperty(WMQConstants.WMQ_CCDTURL, "file://"+ccdtFilePath);
+        }
+
+        if( this.mqCipherSuite!=null && !("".equalsIgnoreCase(this.mqCipherSuite.orElse(""))) ) {
+          cf.setStringProperty(WMQConstants.WMQ_SSL_CIPHER_SUITE, this.mqCipherSuite.orElse(""));
+        }
+
         // Create JMS objects
         jmsContext = cf.createContext();
         destination = jmsContext.createQueue("queue:///" + this.mqQueueName);
-        return jmsContext;	
+        return jmsContext;
     }
 }
