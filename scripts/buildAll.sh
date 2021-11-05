43a54b374cb9d119b0149016db384a891867e867
Jerome Boyer <jerome.boyer@gmail.com>
2021-11-04 17:29:59 -0700

    change build script

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -2,7 +2,8 @@
 scriptDir=$(dirname $0)
 
 IMAGE_NAME=quay.io/ibmcase/eda-store-simulator
-mvn -U clean install -pl :store-simulator-frontend
-mvn -U clean install -pl :store-simulator-backend -DskipTests
+mvn -U clean package -pl :store-simulator-frontend
+cp  -r frontend/target/dist backend/src/main/resources/META-INF/resources
+mvn -U clean package -pl :store-simulator-backend -DskipTests
 docker build -f backend/src/main/docker/Dockerfile.jvm -t ${IMAGE_NAME} backend
 docker push ${IMAGE_NAME}
