98c2065035ad3eb6e065715c717e37addfef3731
jbcodeforce <jerome.boyer@gmail.com>
2022-02-18 15:20:48 -0800

    update to quarkus 2.7.1

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -2,8 +2,10 @@
 scriptDir=$(dirname $0)
 
 IMAGE_NAME=quay.io/ibmcase/eda-store-simulator
+TAG=latest
 mvn -U clean package -pl :store-simulator-frontend
 cp  -r frontend/target/dist backend/src/main/resources/META-INF/resources
 mvn -U clean package -pl :store-simulator-backend -DskipTests
-docker build -f backend/src/main/docker/Dockerfile.jvm -t ${IMAGE_NAME} backend
-docker push ${IMAGE_NAME}
+docker build -f backend/src/main/docker/Dockerfile.jvm -t ${IMAGE_NAME}:${TAG} backend
+docker push ${IMAGE_NAME}:${TAG}
+
